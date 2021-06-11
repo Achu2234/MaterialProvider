25119e0a0309cbd6d738c607bc77c0466886b2e4
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-06-11 14:21:32 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,33 @@
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+
+# Bot information
+SESSION = environ.get('SESSION', 'MaterialProvider')
+USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
+
+# Bot settings
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+
+# Admins, Channels & Users
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+auth_channel = environ.get('AUTH_CHANNEL')
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
+
+# MongoDB information
+DATABASE_URI = environ['DATABASE_URI']
+DATABASE_NAME = environ['DATABASE_NAME']
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+
+START_MSG = environ.get('START_MSG', START_TEXT)
+SHARE_BUTTON_TEXT = '**Checkout {username}, The Best Bot To Provide All Materials Required For Entrance Examinations.**'
+INVITE_MSG = environ.get('INVITE_MSG', '**Please join @Librarian_Official to use this bot**')
